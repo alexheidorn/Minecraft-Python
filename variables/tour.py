016c0ec927fc324621e61999a584fbd905364e3e
alexheidorn <alexheidorn12@gmail.com>
2023-11-26 19:47:24 -0600

    multiple tp's

---
+++ variables/tour.py
@@ -0,0 +1,22 @@
+from mcpi.minecraft import Minecraft
+mc = Minecraft.create()
+import time #import is like the "#include" in C++
+
+#set x, y, z variable to your coords
+x = 10
+y = 110
+z = 12
+
+#change the player's position
+mc.player.setTilePos(x, y, z)
+
+# Wait 10 seconds
+time.sleep(10)
+
+# Do everything again, but with different coords
+x = 100
+y = 64
+z = 200
+
+#change the player's position
+mc.player.setTilePos(x, y, z)
