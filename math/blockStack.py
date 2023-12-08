c9553b398274b46a839b4656d9cd918ecd65c4e7
Alex Heidorn <alexheidorn12@gmail.com>
2023-12-07 20:26:53 -0600

    making 2 stack (and fixing variable so i can see them)

--- math/blockStack.py
+++ math/blockStack.py
@@ -1,8 +1,11 @@
 from mcpi.minecraft import Minecraft
 mc = Minecraft.create()
 
-x = 6
-y = 5
-z = 28
+x = 7
+y = 1
+z = 5
 blockType = 103 # melon
 mc.setBlock(x, y, z, blockType) 
+
+y = y + 1
+mc.setBlock(x, y, z, blockType)
